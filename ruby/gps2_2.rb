caec77b36e759c72d6947554ff83239bd36cb836
Michelle Kelly <michellepk89@gmail.com>
2016-05-01 22:06:23 -0500

    add method for remove, update quantity, and print list

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -31,32 +31,41 @@ def add_item(list, item)
         p list
 end
 
-add_item(list, "banana")
-
-
-# Method to add an item to a list
-# input: item name and optional quantity
-# steps: set a new key with its value to the hash
-# output: print the grocery list hash with the new item 
-
-#def add_item(item, quantity)
-#		grocery_items[:item] = quantity
-#end
-
-#add_item("banana", 6)
-
+add_item(list, "bananas")
 
 # Method to remove an item from the list
 # input: key the user wants to remove
 # steps: list.delete(item)
 # output: updated hash withuot the deleted item 
 
+def remove_item(list, item)
+	list.delete(item)
+	p list
+end
+
+remove_item(list, "pizza")
+
 # Method to update the quantity of an item
 # input: item name and new quantity to override
 # steps: call key within the hash and set it to new quantity value
 # output: new list with a new quantity next to an item 
 
+def update_quantity(list, item, quantity)
+	list[item] = quantity
+	p list
+end
+
+update_quantity(list, "bananas", 8)
+
 # Method to print a list and make it look pretty
 # input: call each on the hash 
 # steps: have method print statement of key and value for each hash item
 # output: pretty list of items that state their quantity 
+
+def print_list(list)
+	list.each do |item, quantity|
+		puts "You need to buy #{quantity} #{item}. "
+	end
+end
+
+print_list(list)
